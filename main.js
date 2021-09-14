378c513353b81a5d4a8c4382abdfbc58efde0ea0
XiangyuShen <xshen2006@yahoo.com>
2021-09-14 13:02:17 -0400

    Ball refactored and drawn

--- main.js
+++ main.js
@@ -1,21 +1,17 @@
-import Block from "./model/Block.js";
-import Sprite from "./model/Sprite.js";
+import Ball from "./model/Ball.js";
 import './style.css'
 
 const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d");
 
-const redBlock = new Block(20, 40, 50, 50, "#FF0000");
+const ball = new Ball(canvas.width / 2, canvas.height - 30, 10, 10, "#0095DD", 2, -2);
 
-const blueSprite = new Sprite(
- canvas.width / 2,
- canvas.height - 30, 10, 10, "#0095DD", 2, -2);
 function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  redBlock.draw(ctx);
-
-  blueSprite.draw(ctx);
-  blueSprite.move();
+  
+  ball.draw(ctx);
+  ball.move();
+  ball.bounce(canvas.width, canvas.height);
 
   window.requestAnimationFrame(draw);
 }
