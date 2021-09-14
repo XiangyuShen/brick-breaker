391bce7e3630b922c901be2df046743ae70aa036
XiangyuShen <xshen2006@yahoo.com>
2021-09-14 13:06:14 -0400

    Paddle class created and drawn

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 import Ball from "./model/Ball.js";
+import Paddle from "./model/Paddle.js";
+
 import './style.css'
 
 const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d");
 
 const ball = new Ball(canvas.width / 2, canvas.height - 30, 10, 10, "#0095DD", 2, -2);
+const paddle = new Paddle((canvas.width - 10) / 2, canvas.height - 10, 75, 10, "#0095DD");
 
 function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -12,6 +15,9 @@ function draw() {
   ball.draw(ctx);
   ball.move();
   ball.bounce(canvas.width, canvas.height);
+
+  paddle.draw(ctx);
+  paddle.move();
 
   window.requestAnimationFrame(draw);
 }
