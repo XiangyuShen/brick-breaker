72b247e042bb665c5c385b18cb559f960ef60141
XiangyuShen <xshen2006@yahoo.com>
2021-09-14 12:52:01 -0400

    Block and Sprite classes created and demo

--- main.js
+++ main.js
@@ -1,6 +1,23 @@
+import Block from "./model/Block.js";
+import Sprite from "./model/Sprite.js";
 import './style.css'
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+const canvas = document.getElementById("myCanvas");
+const ctx = canvas.getContext("2d");
+
+const redBlock = new Block(20, 40, 50, 50, "#FF0000");
+
+const blueSprite = new Sprite(
+ canvas.width / 2,
+ canvas.height - 30, 10, 10, "#0095DD", 2, -2);
+function draw() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  redBlock.draw(ctx);
+
+  blueSprite.draw(ctx);
+  blueSprite.move();
+
+  window.requestAnimationFrame(draw);
+}
+
+draw();
