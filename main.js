687372370a45f91764c84e5d68306ecba9e655a0
XiangyuShen <xshen2006@yahoo.com>
2021-09-14 13:16:36 -0400

    Added bricks to break with ball.

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import Ball from "./model/Ball.js";
 import Paddle from "./model/Paddle.js";
+import Brick from "./model/Brick.js";
 
 import './style.css'
 
@@ -8,6 +9,23 @@ const ctx = canvas.getContext("2d");
 
 const ball = new Ball(canvas.width / 2, canvas.height - 30, 10, 10, "#0095DD", 2, -2);
 const paddle = new Paddle((canvas.width - 10) / 2, canvas.height - 10, 75, 10, "#0095DD");
+
+const brickRowCount = 3;
+const brickColumnCount = 5;
+const bricks = [];
+const brickWidth = 75;
+const brickHeight = 20;
+const brickPadding = 10;
+const brickOffsetTop = 30;
+const brickOffsetLeft = 30;
+
+for (let c = 0; c < brickColumnCount; c++) {
+  for (let r = 0; r < brickRowCount; r++) {
+    let brickX = c * (brickWidth + brickPadding) + brickOffsetLeft;
+    let brickY = r * (brickHeight + brickPadding) + brickOffsetTop;
+    bricks.push(new Brick(brickX, brickY, brickWidth, brickHeight, "#0095DD"));
+  }
+}
 
 let isGameOver = false;
 function draw() {
@@ -20,9 +38,14 @@ function draw() {
   paddle.draw(ctx);
   paddle.move(canvas.width);
 
+  bricks.forEach((brick) => {
+    brick.draw(ctx);
+    brick.colides(ball);
+  });
+
   if (!isGameOver) {
     window.requestAnimationFrame(draw);
-} else {
+  } else {
     window.alert("Game over!");
   }
 }
